e4b415d245849a84a93fa2eba9de4c51c3a974a9
ZainWD <sonicfanjackson@gmail.com>
2018-02-02 20:56:57 -0500

    h

--- Freddy-s-Acronical-Excursion-MV-5d4048b5ab6f33567c5523259a964879612ca87b/js/plugins/regainbattletint.js
+++
@@ -1,9 +0,0 @@
-Scene_Battle.prototype.terminate = function() {
-    Scene_Base.prototype.terminate.call(this);
-    $gameParty.onBattleEnd();
-    $gameTroop.onBattleEnd();
-    AudioManager.stopMe();
-
-    ImageManager.clearRequest();
-	$gameScreen.startTint([-85,-51,-34,170], 1);
-};
